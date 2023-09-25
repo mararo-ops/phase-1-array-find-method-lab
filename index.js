e7e8e23e10c5c1f22620e167ad94ff4d743a0b49
mararo-ops <danielmararo1@gmail.com>
2023-09-25 20:31:15 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// code your solution here
+function superbowlWin(records) {
+    const finder = records.find(record => record.result === "W");
+    return finder ? finder.year : undefined;
+}
+const broncosRecords = [
+    { year: "2015", result: "W" },
+    { year: "2014", result: "N/A" },
+    { year: "2013", result: "L" },
+   
+  ];
+  
+  console.log(superbowlWin(broncosRecords));
